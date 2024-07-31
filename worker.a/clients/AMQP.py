02d37c9cd7f26d74b23bb6f863a465d07f4f66f8
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-07-31 09:51:25 +0300

    refactor: replace queue numerical naming with alphabetical naming

--- worker.a/clients/AMQP.py
+++ worker.a/clients/AMQP.py
@@ -3,20 +3,20 @@ import pika
 
 
 class Client:
-    q1_name = os.getenv('QUEUE_1_NAME', 'queue_1')
-    q2_name = os.getenv('QUEUE_2_NAME', 'queue_2')
+    queue_a_name = os.getenv('QUEUE_A_NAME', 'queue_a')
+    queue_b_name = os.getenv('QUEUE_B_NAME', 'queue_b')
     def __init__(self, queue_name):
         self.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-        self.ch1 = self.connection.channel()
-        self.ch1.queue_declare(queue=self.q1_name, durable=True, exclusive=False, auto_delete=False)
-        self.ch2 = self.connection.channel()
-        self.ch2.queue_declare(queue=self.q2_name, durable=True, exclusive=False, auto_delete=False)
-        self.consumer_tag = 'worker_1'
+        self.channel_a = self.connection.channel()
+        self.channel_a.queue_declare(queue=self.queue_a_name, durable=True, exclusive=False, auto_delete=False)
+        self.channel_b = self.connection.channel()
+        self.channel_b.queue_declare(queue=self.queue_b_name, durable=True, exclusive=False, auto_delete=False)
+        self.consumer_tag = 'worker_a'
 
     def consume(self, callback, auto_ack=False, max_messages=1):
-        self.ch1.basic_qos(prefetch_count=max_messages, global_qos=False)
-        self.ch1.basic_consume(queue=self.q1_name, on_message_callback=callback, auto_ack=auto_ack, consumer_tag=self.consumer_tag)
-        self.ch1.start_consuming()
+        self.channel_a.basic_qos(prefetch_count=max_messages, global_qos=False)
+        self.channel_a.basic_consume(queue=self.queue_a_name, on_message_callback=callback, auto_ack=auto_ack, consumer_tag=self.consumer_tag)
+        self.channel_a.start_consuming()
 
     def publish(self, message):
         pass
